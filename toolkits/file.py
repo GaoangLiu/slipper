03442c7af33c0c18815d13b9a16c89f9d3738707
Gaoang <GaoangLau@gmail.com>
2021-02-28 12:46:19 +0800

    fix(toolkits.file): 写文件权限提示

--- toolkits/file.py
+++ toolkits/file.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from dofast.utils import textwrite, textread
+from getpass import getpass
 
 
 def load_password(file_path: str) -> str:
@@ -10,6 +11,11 @@ def load_password(file_path: str) -> str:
     if os.path.exists(file_path):
         return textread(file_path)[0]
     else:
-        _passphrase = getpass("Type in your passphrase: ")
-        textwrite(_passphrase, file_path)  # sudo may required.
+        _passphrase = getpass("Type here your OSS passphrase: ")
+        if os.geteuid() != 0:
+            print(
+                "To store the password, you need to run this script with sudo!"
+            )
+        else:
+            textwrite(_passphrase, file_path)
         return _passphrase
