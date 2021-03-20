67f83bdffae66bf9c56df7797530e0085a6acc88
Gaoang <GaoangLau@gmail.com>
2021-03-20 13:21:23 +0800

    perf(concurrency): 优化并发框架，通过 tag 来定位线程

--- dofast/concurrency.py
+++ dofast/concurrency.py
@@ -1,38 +1,64 @@
 import time
 import threading
 import queue
-from typing import List
+from collections import defaultdict
+from typing import List, Tuple
+from functools import partial
 
 
 class Concurrency:
     def __init__(self):
         self._threads = {}
-        self._results = []  # store return result
+        self._tags = set()
+        self.results = defaultdict(None)  # store return result
 
-    def create_task(self, func, *args, timeout: float = float('inf')):
-        t = threading.Thread(target=lambda q, a: q.append(func(*a)),
-                             args=(self._results, args),
+    def _add_tag(self, tag: str) -> str:
+        """Add a tag for a thread. A thread tag works as a thread ID,
+        but added before a thread is created.
+        """
+        if not tag or tag in self._tags:
+            tag += str(time.perf_counter())
+        self._tags.add(tag)
+        return tag
+
+    def is_thread_complete(self, tag: [str, List[str]]):
+        '''check thread completeness by its tag'''
+        def is_done(r: str) -> bool:
+            return r in self.results
+
+        return is_done(tag) if isinstance(tag, str) else all(map(is_done, tag))
+
+    def create_task(self,
+                    func: str,
+                    *args,
+                    timeout: float = float('inf'),
+                    tag: str = "") -> None:
+        tag = self._add_tag(tag or str(func))
+        t = threading.Thread(target=lambda q, a: q.update({tag: func(*a)}),
+                             args=(self.results, args),
                              daemon=True)
         t.start()
         self._threads[t] = timeout
 
-    def close(self):
+    def close(self) -> None:
+        _sleep_period = 0.01
         while len(self._threads) > 0:
-            time.sleep(1)
+            time.sleep(_sleep_period)
+
             for t in list(self._threads.keys()):
-                self._threads[t] -= 1
+                self._threads[t] -= _sleep_period
                 if self._threads[t] <= 0:
                     t.join(timeout=0)
                     del self._threads[t]
 
-                if not t.is_alive():
+                if t in self._threads and not t.is_alive():
                     del self._threads[t]
 
-        for v in self._results:
-            print(v)
-
-    def results(self) -> List:
-        return self._results
+    def __repr__(self):
+        res = ''
+        for t in self._tags:
+            res += f'Thread tag: {t} | thread result: {self.results.get(t, None)}\n'
+        return res
 
 
 def ss(tt: int):
