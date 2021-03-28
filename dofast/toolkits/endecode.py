1968c2115d8632a7fabefb21aea020e5ecd97433
GaoangLau <ssruoz@gmail.com>
2021-03-28 13:18:54 +0800

    perf(toolkit.endecode): short_encode 代码重构

--- dofast/toolkits/endecode.py
+++ dofast/toolkits/endecode.py
@@ -6,17 +6,17 @@ from Crypto.Cipher import AES
 from itertools import dropwhile
 
 
-def encode(msg_text: str, passphrase: str) -> str:
-    msg_text += '|' + msg_text * 100
-    msg_text = msg_text[:1024]
-    bytes_text = msg_text.encode().rjust(1024)  # in case this is a long text
+def encode(message: str, passphrase: str) -> str:
+    message += '|' + message * 100
+    message = message[:1024]
+    bytes_text = message.encode().rjust(1024)  # in case this is a long text
     passphrase = passphrase.encode().ljust(32, b'*')
     cipher = AES.new(passphrase, AES.MODE_ECB)
     return base64.b64encode(cipher.encrypt(bytes_text)).decode('ascii')
 
 
-def decode(msg_text: str, passphrase: str) -> str:
-    bytes_text = msg_text.encode().ljust(1024)
+def decode(message: str, passphrase: str) -> str:
+    bytes_text = message.encode().ljust(1024)
     passphrase = passphrase.encode().ljust(32, b'*')
     try:
         cipher = AES.new(passphrase, AES.MODE_ECB)
@@ -27,28 +27,29 @@ def decode(msg_text: str, passphrase: str) -> str:
         return ""
 
 
-def short_encode(msg_text: str, passphrase: str) -> str:
-    """Automatically increase just length w.r.t. msg_text"""
-    _len = 4**next(dropwhile(lambda x: 4**x < len(msg_text), range(3, 1000)))
-    msg_text += '|' + msg_text * _len
-    msg_text = msg_text[:_len]
-    bytes_text = msg_text.encode().rjust(_len)  # in case this is a long text
+def short_encode(message: str, passphrase: str) -> str:
+    """Automatically increase just length w.r.t. message"""
+    _bytelen, _len = 2, 0
+    while 4**_bytelen < len(message):
+        _bytelen += 1
+    _len = 4**_bytelen
+    bytes_text = message.encode().ljust(_len, b'|')
     passphrase = passphrase.encode().ljust(32, b'*')
     cipher = AES.new(passphrase, AES.MODE_ECB)
     return base64.b64encode(
         cipher.encrypt(bytes_text)).decode('ascii') + "|" + str(_len)
 
 
-def short_decode(msg_text: str, passphrase: str) -> str:
-    if '|' not in msg_text:
-        return decode(msg_text, passphrase)
-    _text, _len = msg_text.split('|')
+def short_decode(message: str, passphrase: str) -> str:
+    if '|' not in message:
+        return decode(message, passphrase)
+    _text, _len = message.split('|')
     bytes_text = _text.encode().ljust(int(_len))
     passphrase = passphrase.encode().ljust(32, b'*')
     try:
         cipher = AES.new(passphrase, AES.MODE_ECB)
         return cipher.decrypt(
-            base64.b64decode(bytes_text)).decode().split('|')[0]
+            base64.b64decode(bytes_text)).decode().split('|')[0].strip()
     except Exception as e:
         print(f"Maybe wrong password. {e}")
         return ""
@@ -59,3 +60,6 @@ def decode_with_keyfile(file_path: str, encrypted_message: str) -> str:
     with open(file_path, 'r') as f:
         _decode = short_decode if '|' in encrypted_message else decode
         return _decode(encrypted_message, f.read().strip())
+
+
+print(short_decode(rs, 'passport'))
